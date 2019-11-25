3f8633432a33139b3bf1e3b11afdd03a7f8bd4e1
weynsa <weyns.arno@gmail.com>
2019-11-25 23:11:23 +0100

    db

---
+++ src/com/uantwerpen/Objects/GroupMember.java
@@ -0,0 +1,41 @@
+package com.uantwerpen.Objects;
+
+public class GroupMember {
+    public String Name;
+    public String Email;
+    public String Group;
+    public int Saldo;
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getGroup() {
+        return Group;
+    }
+
+    public void setGroup(String Group) {
+        this.Group = Group;
+    }
+
+    public int getSaldo() {
+        return Saldo;
+    }
+
+    public void setSaldo(int Saldo) {
+        this.Saldo = Saldo;
+    }
+}
